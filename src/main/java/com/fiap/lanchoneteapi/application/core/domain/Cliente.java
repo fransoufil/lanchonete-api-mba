1c2cb5826d3df15cd728f8288a35680a53c51b8a
fransoufil <fran.sf@uol.com.br>
2024-05-23 20:00:35 -0300

    Domain Cliente.

--- src/main/java/com/fiap/lanchoneteapi/application/core/domain/Cliente.java
+++ src/main/java/com/fiap/lanchoneteapi/application/core/domain/Cliente.java
@@ -1,4 +1,73 @@
 package com.fiap.lanchoneteapi.application.core.domain;
 
+import java.util.Objects;
+
 public class Cliente {
+
+    private Integer id;
+    private String cpf;
+    private String nome;
+    private String email;
+
+    public Cliente(Integer id, String cpf, String nome, String email) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id) && Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
