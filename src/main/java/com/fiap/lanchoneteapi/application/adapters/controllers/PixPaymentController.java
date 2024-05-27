df103d29a96342f32328dc6137d1e4fe72a4a170
fransoufil <fran.sf@uol.com.br>
2024-05-27 17:41:19 -0300

    MercadoPago aplicado.

---
+++ src/main/java/com/fiap/lanchoneteapi/application/adapters/controllers/PixPaymentController.java
@@ -0,0 +1,30 @@
+package com.fiap.lanchoneteapi.application.adapters.controllers;
+
+import com.fiap.lanchoneteapi.application.entities.dtos.PixPaymentDTO;
+import com.fiap.lanchoneteapi.application.entities.dtos.PixPaymentResponseDTO;
+import com.fiap.lanchoneteapi.infrastructure.service.PixPaymentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/lanchonete")
+public class PixPaymentController {
+
+    private final PixPaymentService pixPaymentService;
+
+    @Autowired
+    public PixPaymentController(PixPaymentService pixPaymentService) {
+        this.pixPaymentService = pixPaymentService;
+    }
+
+    @PostMapping(value = "/process_payment")
+    public ResponseEntity<PixPaymentResponseDTO> processPayment(@RequestBody @Valid PixPaymentDTO pixPaymentDTO) {
+        PixPaymentResponseDTO payment = pixPaymentService.processPayment(pixPaymentDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(payment);
+    }
+}
---
+++ src/main/java/com/fiap/lanchoneteapi/infrastructure/service/MercadoPagoException.java
@@ -0,0 +1,7 @@
+package com.fiap.lanchoneteapi.infrastructure.service;
+
+public class MercadoPagoException extends RuntimeException {
+    public MercadoPagoException(String message) {
+        super(message);
+    }
+}
